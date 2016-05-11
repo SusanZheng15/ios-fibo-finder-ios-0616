18b40eb9f1bc7beb4111de505348fb53317fb4fe
SusanZheng15 <zheng.susan12@gmail.com>
2016-05-11 12:04:50 -0400

    done

--- FiboFinder/FISAppDelegate.m
+++ FiboFinder/FISAppDelegate.m
@@ -4,15 +4,39 @@
 
 @implementation FISAppDelegate
 
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOption
+{
     // Override point for customization after application launch.
     return YES;
 }
 
-/**
- 
- * Write your method implemenation here.
- 
- */
+- (NSArray *)arrayWithFibonacciSequenceToIndex:(NSUInteger)index
+{
+        NSMutableArray *sequence = [NSMutableArray new];
+        NSUInteger fibo = 0;
+    
+        for (NSUInteger i = 0; i < index + 1; i++)
+        {
+            if (i == 0)
+            {
+                [sequence addObject:@0];
+            }
+             else if (i == 1)
+             {
+                [sequence addObject:@1];
+             }
+             else
+             {
+                 NSUInteger first = [[sequence objectAtIndex:[sequence count] - 1] integerValue];
+                 NSUInteger second = [[sequence objectAtIndex:[sequence count] - 2] integerValue];
+                 fibo = first + second;
+                 NSNumber *addedValues = @(fibo);
+                 [sequence addObject: addedValues];
+             }
+        }
+    
+    NSLog(@"%@", sequence);
 
+return sequence;
+}
 @end
